cc5dd77007cfa06b2e6a184aace9916050574b43
NguyenTanDatHUIT <nguyentandathuit@gmail.com>
2024-09-12 15:27:56 +0700

    add 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 //Dat thay đổi chõ này
  Console.WriteLine("Hello, World!2");
+
+//Dat thay đổi chõ này
+Console.WriteLine("Hello, World!3");
